79b1760fca09afcbd145285ae65521e446733e73
Yayiyi94 <yadiratr94@gmail.com>
2025-06-03 14:26:38 -0600

    Added API to the java script, fixed some misspelling in the classes and id names

--- src/script.js
+++ src/script.js
@@ -1,12 +1,31 @@
+function displayPoem(response) {
+  console.log("Generated");
+
+  new Typewriter("#poem-ai", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 30,
+    cursor: "",
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem-ai", {
-    strings: "this is a test for the real thing",
-    autoStart: true,
-    delay: 20,
-    cursor: "",
-  });
+  let instructionsInput = document.querySelector("#prompt-input");
+
+  let apiKey = "10fa90a2o832483bf734tfe8a27fcdad";
+  let context =
+    "You are a romantic poem expert and your work is to generate only a 6 lines poem in basic HTML, without the HTML lines. Follow the user instructions. Sign at the end of the poem, adding a <br /> element, by Shecodes Ai";
+  let prompt = `User instruccions are: Generate a poem in the language Spanish around the word ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating Poem");
+
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
