b894a9dd5c549fd96daa429d825a24461bc3641e
Yayiyi94 <yadiratr94@gmail.com>
2025-06-03 14:58:44 -0600

    Added User experience, upgrades in the context of the AI

--- src/script.js
+++ src/script.js
@@ -16,9 +16,13 @@ function generatePoem(event) {
 
   let apiKey = "10fa90a2o832483bf734tfe8a27fcdad";
   let context =
-    "You are a romantic poem expert and your work is to generate only a 6 lines poem in basic HTML, without the HTML lines. Follow the user instructions. Sign at the end of the poem, adding a <br /> element, by Shecodes Ai";
-  let prompt = `User instruccions are: Generate a poem in the language Spanish around the word ${instructionsInput.value}`;
+    "You are a romantic poem expert with the knowledge of many languages and your work is to generate only a 6 lines poem in basic HTML, without the HTML lines. Follow the user instructions. Sign at the end of the poem, adding a <br /> element on top of it, as: by Shecodes Ai.";
+  let prompt = `User instruccions are: Generate a poem based on this: ${instructionsInput.value}, make sure to do it in the language that was written.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem-ai");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class = "text-generating"> Generating the poem...</div>`;
 
   console.log("Generating Poem");
 
