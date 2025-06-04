23f6042f3b8f80246441126753ccefe186dcfd5f
Yayiyi94 <yadiratr94@gmail.com>
2025-06-03 21:37:04 -0600

    Change the generator to a gothic one, added input for the lines

--- src/script.js
+++ src/script.js
@@ -1,6 +1,4 @@
 function displayPoem(response) {
-  console.log("Generated");
-
   new Typewriter("#poem-ai", {
     strings: response.data.answer,
     autoStart: true,
@@ -13,21 +11,16 @@ function generatePoem(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#prompt-input");
+  let instructionslinesInput = document.querySelector("#prompt-number-input");
 
   let apiKey = "10fa90a2o832483bf734tfe8a27fcdad";
-  let context =
-    "You are a romantic poem expert with the knowledge of many languages and your work is to generate only a 6 lines poem in basic HTML, without the HTML lines. Follow the user instructions. Sign at the end of the poem, adding a <br /> element on top of it, as: by Shecodes Ai.";
-  let prompt = `User instruccions are: Generate a poem based on this: ${instructionsInput.value}, make sure to do it in the language that was written.`;
+  let context = `You are a gothic poem expert, writing that employs dark and picturesque scenery, startling and melodramatic narrative devices, and an overall atmosphere of exoticism, mystery, fear, and dread, with the knowledge of many languages, and your work is to generate only a ${instructionslinesInput.value} lines poem in basic HTML, taking off the HTML lines. Format the poem using <p> elements. Follow the user instructions. Add an signature at the end of the poem as: by Shecodes Ai with an <br /> element on top of the signature.`;
+  let prompt = `User instruccions are: Generate a gothic poem based on this: ${instructionsInput.value}, make sure to do it in the language that was written.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem-ai");
   poemElement.classList.remove("hidden");
   poemElement.innerHTML = `<div class = "text-generating"> Generating the poem...</div>`;
-
-  console.log("Generating Poem");
-
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
 
   axios.get(apiUrl).then(displayPoem);
 }
